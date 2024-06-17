f9b8f64fad4b6d22b1db582bcdf7970767d812ca
Kenta <kenta.waibel@gmail.com>
2024-06-17 11:06:56 +0200

    Delete remote-config.sh

--- remote-config.sh
+++
@@ -1,9 +0,0 @@
-export MAC_ADDRESS="50:EB:F6:25:2D:FD"
-export ROUTER_PUBLIC_IP="92.105.36.51"
-export PC_LOCAL_IP="192.168.1.124"
-export SSH_USER="kenta"
-export PORT="9"
-export SSH_KEY="~/.ssh/wake_on_lan"
-export APPLICATION="\"D:\\program\\Parsec\\parsecd.exe\""
-
-
